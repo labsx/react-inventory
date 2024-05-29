fcb76f145935e3525763ce9cd1a5b135f7b91ef9
labsx <jrlabrador@revlv.com>
2024-05-29 10:01:19 +0800

    set up laravel with react

---
+++ resources/js/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+// import { TaskProvider } from "./context/TaskContext";
+// import Routes from "./routes/Routes";
+
+const App = () => {
+  return <>Hi im inspect</>
+    // <BrowserRouter>
+    //   <TaskProvider>
+    //     <Routes />
+    //   </TaskProvider>
+    // </BrowserRouter>
+
+};
+
+export default App;
